b2c3c0ae6fe8d0d387a59054ef74010551c90dfd
artiimor <36110555+artiimor@users.noreply.github.com>
2019-10-31 09:42:43 +0100

    Some risks checked and names changed

--- rtl/processor.vhd
+++ rtl/processor.vhd
@@ -96,31 +96,31 @@ signal a3 : std_logic_vector(4 downto 0); --de los registros.
 signal rd1, rd2, aluaux, alures, wd3, sigextend, sigextend_j,  suma4, PCSalida, sl2, sl2_j, muxpc, sumapc : std_logic_vector (31 downto 0);
 
 --para el registro 0
-signal suma4_0, instruccion_0 : std_logic_vector(31 downto 0);
+signal suma4_IFID, instruccion_IFID : std_logic_vector(31 downto 0);
 
 --Para el primer registro todas las señales
-signal regwrite_1, memtoreg_1, branch_1, memread_1, memwrite_1, regdst_1, alusrc_1 : std_logic;
-signal aluop_1: std_logic_vector(2 downto 0);
-signal suma4_1, rd1_1, rd2_1, sigextend_1: std_logic_vector(31 downto 0);
-signal muxwr0_1, muxwr1_1: std_logic_vector(4 downto 0);
+signal regwrite_IDEX, memtoreg_IDEX, branch_IDEX, memread_IDEX, memwrite_IDEX, regdst_IDEX, alusrc_IDEX : std_logic;
+signal aluop_IDEX: std_logic_vector(2 downto 0);
+signal suma4_IDEX, rd1_IDEX, rd2_IDEX, sigextend_IDEX: std_logic_vector(31 downto 0);
+signal muxwr0_IDEX, muxwr1_IDEX: std_logic_vector(4 downto 0);
 
 --Para el segundo registro
-signal branch_2, memread_2, memwrite_2, memtoreg_2, regwrite_2 : std_logic;
-signal zflag_2 : std_logic;
-signal alures_2, sumapc_2, rd2_2: std_logic_vector(31 downto 0);
-signal a3_2: std_logic_vector(4 downto 0);
+signal branch_EXMEM, memread_EXMEM, memwrite_EXMEM, memtoreg_EXMEM, regwrite_EXMEM : std_logic;
+signal zflag_EXMEM : std_logic;
+signal alures_EXMEM, sumapc_EXMEM, rd2_EXMEM: std_logic_vector(31 downto 0);
+signal a3_EXMEM: std_logic_vector(4 downto 0);
 
 --Para el tercer registro
-signal regwrite_3, memtoreg_3 : std_logic;
-signal rd_3, alures_3 : std_logic_vector(31 downto 0);
-signal a3_3: std_logic_vector(4 downto 0);
+signal regwrite_MEMWB, memtoreg_MEMWB : std_logic;
+signal rd_MEMWB, alures_MEMWB : std_logic_vector(31 downto 0);
+signal a3_MEMWB: std_logic_vector(4 downto 0);
 
 --Para los multiplexores de la forwarding unity
 signal aluin_1, aluin_2 : std_logic_vector(31 downto 0);
-signal mux_registerrd, mux_registerrd_2, mux_registerrd_3: std_logic_vector(4 downto 0);
+signal mux_registerrd, mux_registerrd_EXMEM, mux_registerrd_MEMWB: std_logic_vector(4 downto 0);
 
--- Para rs, rt y rd en el caso _1
-signal rs_1, rt_1, rd_1 : std_logic_vector(4 downto 0);
+-- Para rs, rt y rd en el caso _IDEX
+signal rs_IDEX, rt_IDEX, rd_IDEX : std_logic_vector(4 downto 0);
 
 begin 
 
@@ -128,18 +128,18 @@ begin
 u1: reg_bank port map(
 	Clk   => Clk,
  	Reset => Reset,
-  	A1    => instruccion_0(25 downto 21),
+  	A1    => instruccion_IFID(25 downto 21),
   	Rd1   => rd1, --señal
-  	A2    => instruccion_0 (20 downto 16),
+  	A2    => instruccion_IFID (20 downto 16),
   	Rd2   => rd2, --señal
-  	A3    => a3_3, --señal (mux)
+  	A3    => a3_MEMWB, --señal (mux)
 	Wd3   => wd3, --señal (mux)
- 	We3   => regwrite_3 --señal
+ 	We3   => regwrite_MEMWB --señal
 );
 
 --portmap de la alu
 u2: alu port map(
-	OpA     => aluin_1, --antes era rd1_1
+	OpA     => aluin_1, --antes era rd1_IDEX
   	OpB     => aluaux, --señal (mux)
 	Control => alucontrol,
   	Result  => alures,
@@ -147,7 +147,7 @@ u2: alu port map(
 );
 
 u3: control_unit port map(
-	OpCode  => instruccion_0(31 downto 26),
+	OpCode  => instruccion_IFID(31 downto 26),
 	Branch => branch,
 	MemToReg => memtoreg,
 	MemWrite => memwrite,
@@ -159,8 +159,8 @@ u3: control_unit port map(
 );
 
 u4: alu_control port map(
-	ALUOp  => aluop_1,
-	Funct  => sigextend_1(5 downto 0),
+	ALUOp  => aluop_IDEX,
+	Funct  => sigextend_IDEX(5 downto 0),
 	ALUControl => alucontrol
 
 );
@@ -174,120 +174,120 @@ process(Reset, Clk) begin
 		DDataOut   <= X"00000000";
 		IAddr      <= X"00000000";
 		--registro 0
-		suma4_0   <= X"00000000";
-		instruccion_0 <= X"00000000";
+		suma4_IFID   <= X"00000000";
+		instruccion_IFID <= X"00000000";
 		--registro1
-		regwrite_1 <= '0';
-		memtoreg_1 <= '0';
-		branch_1 <= '0';
-		memread_1 <= '1';
-		memwrite_1 <= '0';
-		regdst_1 <= '0';
-		alusrc_1 <= '0';
-		aluop_1 <= "000";
-		suma4_1 <= X"00000000";
+		regwrite_IDEX <= '0';
+		memtoreg_IDEX <= '0';
+		branch_IDEX <= '0';
+		memread_IDEX <= '1';
+		memwrite_IDEX <= '0';
+		regdst_IDEX <= '0';
+		alusrc_IDEX <= '0';
+		aluop_IDEX <= "000";
+		suma4_IDEX <= X"00000000";
 		
-		rd1_1 <= X"00000000";
-		rd2_1 <= X"00000000";
-		sigextend_1 <= X"00000000";
+		rd1_IDEX <= X"00000000";
+		rd2_IDEX <= X"00000000";
+		sigextend_IDEX <= X"00000000";
 		
-		muxwr0_1 <= "00000";
-		muxwr1_1 <= "00000";
-		rs_1 <= "00000";
-		rt_1 <= "00000";
-		rd_1 <= "00000";
+		muxwr0_IDEX <= "00000";
+		muxwr1_IDEX <= "00000";
+		rs_IDEX <= "00000";
+		rt_IDEX <= "00000";
+		rd_IDEX <= "00000";
 		
 		--Para el segundo registro
-    	branch_2 <= '0';
-    	memread_2 <= '1';
-    	memwrite_2 <= '0';
-    	zflag_2 <= '0';
-   		alures_2 <= X"00000000";
-   		sumapc_2 <= X"00000000";
-   		a3_2 <= "00000";
-   		regwrite_2 <= '0';
-   		memtoreg_2 <= '0';
-   		rd2_2 <= X"00000000";
+    	branch_EXMEM <= '0';
+    	memread_EXMEM <= '1';
+    	memwrite_EXMEM <= '0';
+    	zflag_EXMEM <= '0';
+   		alures_EXMEM <= X"00000000";
+   		sumapc_EXMEM <= X"00000000";
+   		a3_EXMEM <= "00000";
+   		regwrite_EXMEM <= '0';
+   		memtoreg_EXMEM <= '0';
+   		rd2_EXMEM <= X"00000000";
     
   		--Para el tercer registro
-  		regwrite_3 <= '0';
- 		memtoreg_3 <= '0';
-  		rd_3 <= X"00000000";
-  		alures_3 <= X"00000000";
-  		a3_3 <= "00000";
+  		regwrite_MEMWB <= '0';
+ 		memtoreg_MEMWB <= '0';
+  		rd_MEMWB <= X"00000000";
+  		alures_MEMWB <= X"00000000";
+  		a3_MEMWB <= "00000";
     
 	elsif rising_edge(Clk) then --Tenemos en cuenta el nop
 		
-		DWrEn      <= memwrite_2;
-		DRdEn	   <= memread_2;
-		DAddr 	   <= alures_2;
-		DDataOut   <= rd2_2;
+		DWrEn      <= memwrite_EXMEM;
+		DRdEn	   <= memread_EXMEM;
+		DAddr 	   <= alures_EXMEM;
+		DDataOut   <= rd2_EXMEM;
 		IAddr      <= muxpc;
 		--registro 0
-	  	suma4_0   <= suma4;
+	  	suma4_IFID   <= suma4;
 		
 		--registro1. Tenemos en cuenta el caso de las instrucciones lw
-		if (instruccion_0(25 downto 21) = rt_1 or instruccion_0(20 downto 26) = rt_1) and memwrite_1 = '1' then
-			regwrite_1 <= '0';
-			memwrite_1 <= '0';
+		if (instruccion_IFID(25 downto 21) = rt_IDEX or instruccion_IFID(20 downto 26) = rt_IDEX) and memwrite_IDEX = '1' then
+			regwrite_IDEX <= '0';
+			memwrite_IDEX <= '0';
 		else
-			regwrite_1 <= regwrite;
-			memwrite_1 <= memwrite;
+			regwrite_IDEX <= regwrite;
+			memwrite_IDEX <= memwrite;
 			PCSalida <= muxpc;
-			instruccion_0 <= IDataIn;
+			instruccion_IFID <= IDataIn;
 		end if;
-		memtoreg_1 <= memtoreg;
-		branch_1 <= branch;
-		memread_1 <= memread;
-		regdst_1 <= regdst;
-		alusrc_1 <= alusrc;
-		aluop_1 <= aluop;
-		suma4_1 <= suma4_0;
+		memtoreg_IDEX <= memtoreg;
+		branch_IDEX <= branch;
+		memread_IDEX <= memread;
+		regdst_IDEX <= regdst;
+		alusrc_IDEX <= alusrc;
+		aluop_IDEX <= aluop;
+		suma4_IDEX <= suma4_IFID;
 		
-		rd1_1 <= rd1;
-		rd2_1 <= rd2;
+		rd1_IDEX <= rd1;
+		rd2_IDEX <= rd2;
 		
-		rs_1 <= instruccion_0(25 downto 21);
-		rt_1 <= instruccion_0(20 downto 16);
-		rd_1 <= instruccion_0(15 downto 11);
+		rs_IDEX <= instruccion_IFID(25 downto 21);
+		rt_IDEX <= instruccion_IFID(20 downto 16);
+		rd_IDEX <= instruccion_IFID(15 downto 11);
 		
 		
-		muxwr0_1 <= instruccion_0 (20 downto 16);
-		muxwr1_1 <= instruccion_0 (15 downto 11);
+		muxwr0_IDEX <= instruccion_IFID (20 downto 16);
+		muxwr1_IDEX <= instruccion_IFID (15 downto 11);
 		
 		--Para el segundo registro
-    	branch_2 <= branch_1;
-    	memread_2 <= memread_1;
-    	memwrite_2 <= memwrite_1;
-    	memtoreg_2 <= memtoreg_1;
-    	regwrite_2 <= regwrite_1;
-    	zflag_2 <= zflag;
+    	branch_EXMEM <= branch_IDEX;
+    	memread_EXMEM <= memread_IDEX;
+    	memwrite_EXMEM <= memwrite_IDEX;
+    	memtoreg_EXMEM <= memtoreg_IDEX;
+    	regwrite_EXMEM <= regwrite_IDEX;
+    	zflag_EXMEM <= zflag;
     
-    	alures_2 <= alures;
-    	sumapc_2 <= sumapc;
-    	a3_2 <= a3;
-		rd2_2 <= rd2_1;
+    	alures_EXMEM <= alures;
+    	sumapc_EXMEM <= sumapc;
+    	a3_EXMEM <= a3;
+		rd2_EXMEM <= rd2_IDEX;
 		
-		mux_registerrd_2 <= mux_registerrd;
+		mux_registerrd_EXMEM <= mux_registerrd;
 		
     
     	--Para el tercer registro
-    	regwrite_3 <= regwrite_2;
-    	memtoreg_3 <= memtoreg_2;
-    	rd_3 <= DDataIn;
-    	alures_3 <= alures_2;
-    	a3_3 <= a3_2;
+    	regwrite_MEMWB <= regwrite_EXMEM;
+    	memtoreg_MEMWB <= memtoreg_EXMEM;
+    	rd_MEMWB <= DDataIn;
+    	alures_MEMWB <= alures_EXMEM;
+    	a3_MEMWB <= a3_EXMEM;
     	
-    	mux_registerrd_3 <= mux_registerrd_2;
+    	mux_registerrd_MEMWB <= mux_registerrd_EXMEM;
 
 		if IDataIn = X"00000000" then
-			instruccion_0 <= X"11111111";
+			instruccion_IFID <= X"11111111";
 			
 	    	end if;
-		if instruccion_0 = X"11111111" then
-			sigextend_1 <= X"00000000";
+		if instruccion_IFID = X"11111111" then
+			sigextend_IDEX <= X"00000000";
 		else
-			sigextend_1 <= sigextend;
+			sigextend_IDEX <= sigextend;
 		end if;
 		
 		
@@ -296,36 +296,36 @@ end process;
      
 		
 --Hacemos el extensor de signo
---sigextend <= std_logic_vector(resize(signed(instruccion_0(15 downto 0)), sigextend'length));
+--sigextend <= std_logic_vector(resize(signed(instruccion_IFID(15 downto 0)), sigextend'length));
 
-sigextend <= "1111111111111111" & instruccion_0(15 downto 0) when instruccion_0(15) = '1' else "0000000000000000" & instruccion_0(15 downto 0);
+sigextend <= "1111111111111111" & instruccion_IFID(15 downto 0) when instruccion_IFID(15) = '1' else "0000000000000000" & instruccion_IFID(15 downto 0);
 --Hacemos el extensor de signo para j
-sigextend_j <= "111111" & instruccion_0(25 downto 0) when instruccion_0(25) = '1' else "000000" & instruccion_0(25 downto 0);
+sigextend_j <= "111111" & instruccion_IFID(25 downto 0) when instruccion_IFID(25) = '1' else "000000" & instruccion_IFID(25 downto 0);
 
 --Hacemos el desplazamiento a la izquierda para el pc
-sl2 <= sigextend_1(29 downto 0) & "00";
+sl2 <= sigextend_IDEX(29 downto 0) & "00";
 --Hacemos el desplazamiento a la izquierda y aniadimos pc4 para j
 sl2_j <= suma4(31 downto 28) & sigextend_j(25 downto 0) & "00";
 --hacemos el and
-and_s <= '1' when (branch_2 = '1' and zflag_2 = '1') else '0';
+and_s <= '1' when (branch_EXMEM = '1' and zflag_EXMEM = '1') else '0';
 --hacemos la j en el caso de que la instruccion sea la j
-cond_j <= '1' when (instruccion_0(31 downto 26)  = "000010") else '0';
+cond_j <= '1' when (instruccion_IFID(31 downto 26)  = "000010") else '0';
 --Hacemos los multiplexores
-a3 <= muxwr1_1 when regdst_1 = '1' else muxwr0_1; --pasar por registro
-aluaux <= sigextend_1 when alusrc_1 = '1' else aluin_2; --antes aluin_2 era src_1
-wd3 <= DDataIn when memtoreg_3 = '1' else alures_3;
-muxpc <= sumapc_2 when and_s = '1' else
+a3 <= muxwr1_IDEX when regdst_IDEX = '1' else muxwr0_IDEX; --pasar por registro
+aluaux <= sigextend_IDEX when alusrc_IDEX = '1' else aluin_2; --antes aluin_2 era src_IDEX
+wd3 <= DDataIn when memtoreg_MEMWB = '1' else alures_MEMWB;
+muxpc <= sumapc_EXMEM when and_s = '1' else
                   sl2_j when cond_j = '1' else suma4;
                   
 --multiplexores de la fowarding unity
-aluin_1 <= wd3 when (regwrite_3 = '1' and a3_3 /= 0 and a3_3 = IDataIn(25 downto 21)) else a3_3 when (regwrite_2 = '1' and a3_2 /= 0 and a3_2 = instruccion_0(25 downto 21)) else rd1_1;
-aluin_2 <= wd3 when (regwrite_3 = '1' and mux_registerrd_3 /= 0 and mux_registerrd_3 = instruccion_0(20 downto 16)) else alures_2 when (regwrite_2 = '1' and mux_registerrd_2 /= 0 and mux_registerrd_2 = instruccion_0(20 downto 16)) else rd2_1;
+aluin_1 <= alures_EXMEM when (regwrite_EXMEM = '1' and a3_EXMEM /= 0 and a3_EXMEM = rs_IDEX) else wd3 when (regwrite_MEMWB = '1' and a3_MEMWB /= 0 and a3_MEMWB = rs_IDEX) else rd1_IDEX;
+aluin_2 <= alures_EXMEM when (regwrite_EXMEM = '1' and a3_EXMEM /= 0 and a3_EXMEM = rt_IDEX) else wd3 when (regwrite_MEMWB = '1' and a3_MEMWB /= 0 and a3_MEMWB = rt_IDEX) else rd2_IDEX;
 
-mux_registerrd <= rt_1 when alucontrol = "000" or alucontrol = "001" or alucontrol = "010" or alucontrol = "011" else instruccion_0(15 downto 11);
+mux_registerrd <= rt_IDEX when alucontrol = "000" or alucontrol = "001" or alucontrol = "010" or alucontrol = "011" else instruccion_IFID(15 downto 11);
 
 --cosas que calculo
 suma4 <= PCSalida + 4; --Al ciclo se le suma 4 a la instrucción
-sumapc <= sl2 + suma4_1;
+sumapc <= sl2 + suma4_IDEX;
  
 end architecture;
 
