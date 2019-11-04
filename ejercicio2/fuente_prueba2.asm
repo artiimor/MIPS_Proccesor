596ab888bed5f4db5296abe27438ae0124cb9c0a
artiimor <36110555+artiimor@users.noreply.github.com>
2019-11-04 23:33:44 +0100

    hace los saltos pero no borra los registros antes de saltar

--- ejercicio2/fuente_prueba2.asm
+++ ejercicio2/fuente_prueba2.asm
@@ -1,6 +1,6 @@
 # Prog de prueba para Practica 2. Ej 2
 
-.data 0
+.data
 num0: .word 1 # posic 0
 num1: .word 2 # posic 4
 num2: .word 4 # posic 8 
@@ -13,7 +13,7 @@ num8: .word 0 # posic 32
 num9: .word 0 # posic 36
 num10: .word 0 # posic 40
 num11: .word 0 # posic 44
-.text 0
+.text
 main:
   # carga num0 a num5 en los registros 9 a 14
   lw $t1, 0($zero) # lw $r9, 0($r0)
